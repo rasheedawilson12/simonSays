6dc83261c421bf6b64d622081cc994e1e23452eb
rasheedawilson12 <rasheedawilson12@gmail.com>
2023-06-26 13:38:09 -0500

    basic game logic in JavaScript

--- app.js
+++ app.js
@@ -6,9 +6,29 @@ let playerSequence = [];
 let level = 0;
 
 // flash colors when tile is activated in the sequence array
+// must activiate the tiles on the screen in the right order
 function flashTile(color) {
-  const title = document.querySelector(`[data-tile='${color}']`);
+  const tile = document.querySelector(`[data-tile='${color}']`);
+
+  //   add activated class to tile to show that it is being engaged by the sequence.
+  tile.classList.add("activated");
+
+  //   remove activated class after 3 milliseconds after it has been activated in the sequence
+  setTimeout(() => {
+    DataTransferItemList.classList.remove("activated");
+  }, 300);
 }
+
+// play round with the sequences engaging and the active class toggling on and off from the flash tile function.
+// playRound passing through new sequence to activate tiles on the screen
+const playRound = (newSequence) => {
+  newSequence.forEach((color, index) => {
+    // flashes each colored title at 600 milisecond intervals until the sequence has concluuded and it is players turn to go.
+    setTimeout(() => {
+      flashTile(color);
+    }, (index + 1) * 600);
+  });
+};
 
 // add step to the existing sequence
 const addStep = () => {
@@ -25,6 +45,10 @@ const nextRound = () => {
 
   //   copy all of the elements in the sequence array into the newSequence array
   let newSequence = [...sequence];
+  //   push the new sequence into the array
+  newSequence.push(addStep());
+  // go through the playRound logic with the new sequnce passed through
+  playRound(newSequence);
 };
 
 // Query Selectors
