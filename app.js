7e0deca250c67c85ef81a0edcc69830849561d62
rasheedawilson12 <rasheedawilson12@gmail.com>
2023-06-26 11:39:41 -0500

    game bord wireframe compelted

---
+++ app.js
@@ -0,0 +1,43 @@
+// start the game
+
+// start the sequence
+// taget tiles and put avalible options in an array
+// generate a random number that is no longer than the array
+
+// media querries
+let startButton = document.querySelector(".startButton");
+let green = document.querySelector(".topLeft");
+let red = document.querySelector(".topRight");
+let yellow = document.querySelector(".bottomLeft");
+let blue = document.querySelector(".bottomRight");
+
+// Start the round
+let level = 0;
+
+// keep track of the sequence for the computer and the player
+const sequence = [];
+const playerSequence = [];
+
+// Next Round
+const nextRound = () => {
+  // increment round number
+  level += 1;
+  const nextSequence = [...sequence];
+  nextSequence.push(nextSequence());
+};
+
+// add a new step to the existing sequence
+// push sequnce to the DOM
+const newSequence = () => {
+  let tiles = [green, red, yellow, blue];
+  let randomSequence = tiles[Math.floor(Math.random() * tiles.length)];
+
+  return randomSequence;
+};
+
+// allow for player input after sequence
+
+// compare computer sequence to player sequence
+// if correct, go to next round
+// after 5 rounds - win state
+// if incorrect, lose state
